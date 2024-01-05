cdd703660ba0fa76d562375d5818aadeb36fec09
JB Hutch <jbhutch01@gmail.com>
2024-01-05 16:30:56 -0500

    Add react query provider

---
+++ app/react-query-provider.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import * as React from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+export default function ReactQueryProvider({ children }: { children: React.ReactNode }) {
+  const [queryClient] = React.useState(() => new QueryClient())
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+}
